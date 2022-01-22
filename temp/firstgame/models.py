62dda28b65669e0ec949d9f3f433f03750746592
baek <dkf1912@gmail>
2022-01-22 17:25:36 +0900

    랜덤 적용

--- temp/firstgame/models.py
+++ temp/firstgame/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class 상식(models.Model):
+    고유번호 = models.IntegerField()
+    문제 = models.CharField(max_length=255)
+    정답 = models.CharField(max_length=255)
+    제한시간 = models.IntegerField()
+    카테고리 = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = '상식'
+        app_label = 'secondapp'
+        ordering = ['id']
+        managed = False
+
+    def __str__(self):
+        return '@@@' + self.name  
--- temp/firstgame/views.py
+++ temp/firstgame/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
+from firstgame.models import * # 커리큘럼 클래스 사용
+import random
+# Create your views here.
+
+def game1(request):
+    rand = random.randint(1,10)
+    result = ""
+    #sangsick = 상식.objects.all()
+    sangsick = 상식.objects.get(고유번호 = rand)
+    result = '%s.%s --------- %s < %s >' % (sangsick.고유번호, sangsick.문제, sangsick.정답, sangsick.카테고리)
+    return HttpResponse(result)
